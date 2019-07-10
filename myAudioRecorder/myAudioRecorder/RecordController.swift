31d77b86f62c4f0eddbbc25a929f127b15af2110
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-09 23:12:21 -0700

    record Controller

---
+++ myAudioRecorder/myAudioRecorder/RecordController.swift
@@ -0,0 +1,34 @@
+//
+//  RecordController.swift
+//  myAudioRecorder
+//
+//  Created by Hector Steven on 7/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class RecordController {
+	var record: [Record] = []
+	
+	func fetchRecords() {
+	
+		let fetchRequest: NSFetchRequest<Record> = Record.fetchRequest()
+//		fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+		let fetchResultController =  NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.shared.mainContext, sectionNameKeyPath: nil, cacheName: nil)
+		
+		CoreDataStack.shared.mainContext.performAndWait {
+			do {
+				try fetchResultController.performFetch()
+				record = fetchResultController.fetchedObjects ??  []
+				
+			} catch {
+				NSLog("Error fetching results from store: \(error)")
+			}
+		}
+	}
+
+	
+	
+}
