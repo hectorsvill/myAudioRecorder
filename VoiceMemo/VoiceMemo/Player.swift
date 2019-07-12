46dea291540488485d9b59eb25e2f57d05c44c69
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-11 23:03:50 -0700

    Player

---
+++ VoiceMemo/VoiceMemo/Player.swift
@@ -0,0 +1,75 @@
+//
+//  Player.swift
+//  VoiceMemo
+//
+//  Created by Hector Steven on 7/11/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import AVFoundation
+
+class Player: NSObject {
+	let name: String
+	private var audioPlayer: AVAudioPlayer?
+	var timer: Timer?
+	
+	init(name: String) {
+		self.name = name
+	}
+	
+	func setupPlayer() {
+		
+		let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+		
+		let url = documentsDirectory.appendingPathComponent(name).appendingPathExtension("caf")
+		
+		do {
+			audioPlayer = try AVAudioPlayer(contentsOf: url)
+		} catch {
+			NSLog("audioPlayer: \(error)")
+		}
+		audioPlayer?.delegate = self
+		audioPlayer?.play()
+		timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { _ in
+			self.updateTimer()
+		}
+	}
+	func updateTimer() {
+//		NotificationCenter.default.post(name: .timerChangedValue, object: nil)
+	}
+	
+	var isPlaying: Bool {
+		return audioPlayer?.isPlaying ?? false
+	}
+	
+	var elapsedTime: TimeInterval? {
+		return audioPlayer?.currentTime
+	}
+	
+	var duration: TimeInterval? {
+		return audioPlayer?.duration
+	}
+	
+	func play() {
+		audioPlayer?.play()
+		//starttimeer
+	}
+	
+	func pause() {
+		audioPlayer?.pause()
+		timer?.invalidate()
+		timer = nil
+	}
+}
+
+extension Player: AVAudioPlayerDelegate {
+	func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+//		timer?.invalidate()
+//		timer = nil
+		NotificationCenter.default.post(name: .audioPlayerDidFinishPlaying, object: nil)
+	}
+}
+
+extension Notification.Name {
+	static let audioPlayerDidFinishPlaying =  Notification.Name("AudioPlayerDidFinishPlaying")
+}
